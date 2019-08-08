036ac00e0a166d266302c75be58f9ae897ce98da
danshapero <shapero.daniel@gmail.com>
2019-08-08 08:44:54 -0700

    Using hook to pass image name for automated build
    We need to pass the argument `BASE_IMAGE` to docker build in order to
    specify whether we're building on Debian stretch or buster, but
    dockerhub's automated builds don't let you do this directly. This is a
    workaround per [this issue](https://github.com/docker/hub-feedback/issues/508).

--- build-images.sh
+++
@@ -1,2 +0,0 @@
-docker build --tag icepack/firedrake-python3.5:0.1.0 --build-arg BASE_IMAGE=buildpack-deps:stretch --file Dockerfile .
-docker build --tag icepack/firedrake-python3.7:0.1.0 --build-arg BASE_IMAGE=buildpack-deps:buster --file Dockerfile .
